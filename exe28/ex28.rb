ed92adc232b5d6be9b5728328f0d774804cba4d2
nzaga <nzagajunior@gmail.com>
2015-09-22 23:56:50 +0300

    Add exe21-30

---
+++ exe28/ex28.rb
@@ -0,0 +1,90 @@
+# true && true => true 
+puts true && true
+
+#false && true => false
+puts false && true 
+
+# 1 == 1 && 2 == 1 => false
+puts 1 == 1 && 2 == 1
+
+#"test" == "test" => true
+puts "test" == "test"
+
+# 1 == 1 || 2 != 1 => true
+puts 1 == 1 || 2 != 1
+
+# true && 1 == 1 => true
+puts true && 1 == 1
+
+# false && 0 != 0 => false
+puts false && 0 != 0 
+
+#true || 1 == 1 => true
+puts true || 1 == 1
+
+# "test" != "testing" => true
+puts "test" != "testing"
+
+# 1 != 0 && 2 == 1 => false
+puts 1 != 0 && 2 == 1
+
+# "test" != "testing" => true
+puts "test" != "testing"
+
+#"test" == 1 => false
+puts "test" == 1
+
+# !( true && false) => true
+puts !( true && false)
+
+# !( 1 == 1 && 0 != 1) => false
+puts !( 1 == 1 && 0 != 1)
+
+# !( 10 == 1 || 1000 == 1000) => false
+puts !( 10 == 1 || 1000 == 1000)
+
+#!("testing" == "testing" && "Zed" == "Cool Guy") => true
+puts !("testing" == "testing" && "Zed" == "Cool Guy")
+
+# 1 == 1 && (!("testing" == 1 || 1 == 0)) => true
+puts 1 == 1 && (!("testing" == 1 || 1 == 0))
+
+#"chunky" == "bacon" && (! ( 3 == 4 || 3 == 3)) =>false
+puts "chunky" == "bacon" && (! ( 3 == 4 || 3 == 3))
+
+# 3 == 3 && ( ! ("testing" == "testing" || "Ruby" == "Fun")) => false
+puts 3 == 3 && ( ! ("testing" == "testing" || "Ruby" == "Fun"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
